604af4eb4897d12f8e7ec551a263cc973aa23c75
Geovanny Buitrago <geovanny91_1@hotmail.com>
2019-08-27 22:00:57 -0500

    Added Base Activity.

---
+++ app/src/main/java/com/gyros/restapimvvm/BaseActivity.java
@@ -0,0 +1,17 @@
+package com.gyros.restapimvvm;
+
+import android.widget.FrameLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    @Override
+    public void setContentView(int layoutResID) {
+        ConstraintLayout constraintLayout = (ConstraintLayout) getLayoutInflater().inflate(R.layout.activity_base,null);
+        FrameLayout frameLayout = constraintLayout.findViewById(R.id.activity_content);
+        getLayoutInflater().inflate(layoutResID,frameLayout,true);
+        super.setContentView(layoutResID);
+    }
+}
